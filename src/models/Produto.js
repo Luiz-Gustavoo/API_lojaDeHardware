96d7ad266879a1e27a39ec8afcfa2ed41cd974a4
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-03-12 23:10:29 -0300

    colocado required no campo nome do modelo

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const produtoSchema = new mongoose.Schema({
-    id: {type: mongoose.Schema.Types.ObjectId},
-    nome: {type: String},
-    marca: {type: String},
-    preco: {type: Number},
-    estoque: {type: Number},
-    categoria: {type: String}
+  id: {type: mongoose.Schema.Types.ObjectId},
+  nome: {type: String, required: true},
+  marca: {type: String},
+  preco: {type: Number},
+  estoque: {type: Number},
+  categoria: {type: String}
 }, {versionKey: false});
 
-const produto = mongoose.model('hardwares', produtoSchema);
+const produto = mongoose.model("hardwares", produtoSchema);
 
 export default produto;
