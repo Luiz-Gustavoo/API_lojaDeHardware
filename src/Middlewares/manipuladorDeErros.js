e9b97404e50a4c2a7fb7f34354596c256dc6ceab
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-03-12 23:09:53 -0300

    Adicionado erro de validação

--- src/Middlewares/manipuladorDeErros.js
+++ src/Middlewares/manipuladorDeErros.js
@@ -1,10 +1,19 @@
-/* eslint-disable no-unused-vars */
+
 import mongoose from "mongoose";
 
+// eslint-disable-next-line no-unused-vars
 function ManipuladorDeErros(error, req, res, next) {
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).send({mensagem: "Um ou mais dados fornecidos estão incorretos."});
-  } else {
+  } 
+  else if (error instanceof mongoose.Error.ValidationError) {
+    let mensagensErro = "";
+    for (let erro in error.errors) {
+      mensagensErro += `mensagemErro: ${error.errors[erro].message + "; "}`;
+    }
+    res.status(400).send({mensagem: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    
+  }else {
     res.status(500).send({mensagem: "Erro interno do servidor."});
   }
 }
