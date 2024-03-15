33e2f2b7123868d67bd75c4cbfa1b1a0590dea81
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-03-14 23:06:00 -0300

    colocando required e mensagens para todas as propriedades do modelo

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const produtoSchema = new mongoose.Schema({
   id: {type: mongoose.Schema.Types.ObjectId},
-  nome: {type: String, required: true},
-  marca: {type: String},
-  preco: {type: Number},
-  estoque: {type: Number},
-  categoria: {type: String}
+  nome: {type: String, required: [true, "O nome do produto é obrigatório"]},
+  marca: {type: String, required: [true, "A marca do produto é obrigatória"]},
+  preco: {type: Number, required: [true, "o preço do produto é obrigatório"]},
+  estoque: {type: Number, required: [true, "A quantidade em estoque do produto é obrigatória"]},
+  categoria: {type: String, required: [true, "A categoria do produto é obrigatória"]}
 }, {versionKey: false});
 
 const produto = mongoose.model("hardwares", produtoSchema);
