2c1bd9b2a7726d302e73bfd6d5a3b8e535710a60
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-03-14 23:04:18 -0300

    middleware para manipular o erro 404

---
+++ src/Middlewares/manipulador404.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+function manipulador404(req, res, next) {
+  res.status(404).send({mensagem: "Página não encontrada"});
+}
+
+export default manipulador404;
