4c22e0be6e87b20309669830d78656db8d7bd9f9
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-04-06 19:43:03 -0300

    adicionando os status dos erros junto com as mensagens

--- src/Middlewares/manipuladorDeErros.js
+++ src/Middlewares/manipuladorDeErros.js
@@ -4,17 +4,17 @@ import mongoose from "mongoose";
 // eslint-disable-next-line no-unused-vars
 function ManipuladorDeErros(error, req, res, next) {
   if (error instanceof mongoose.Error.CastError) {
-    res.status(400).send({mensagem: "Um ou mais dados fornecidos estão incorretos."});
+    res.status(400).send({mensagem: "Um ou mais dados fornecidos estão incorretos.", status: 400});
   } 
   else if (error instanceof mongoose.Error.ValidationError) {
     let mensagensErro = "";
     for (let erro in error.errors) {
       mensagensErro += `${error.errors[erro].message + "; "}`;
     }
-    res.status(400).send({mensagem: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    res.status(400).send({mensagem: `Os seguintes erros foram encontrados: ${mensagensErro}`, status: 400});
     
   }else {
-    res.status(500).send({mensagem: "Erro interno do servidor."});
+    res.status(500).send({mensagem: "Erro interno do servidor.", status: 500});
   }
 }
 
