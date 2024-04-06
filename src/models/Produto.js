38f6fe15aaa1a36a0a7ee133c0ddc2c65e10bd98
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-04-06 18:42:11 -0300

    formatação das linhas

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -1,13 +1,38 @@
 import mongoose from "mongoose";
 
-const produtoSchema = new mongoose.Schema({
-  id: {type: mongoose.Schema.Types.ObjectId},
-  nome: {type: String, required: [true, "O nome do produto é obrigatório"]},
-  marca: {type: String, required: [true, "A marca do produto é obrigatória"]},
-  preco: {type: Number, required: [true, "o preço do produto é obrigatório"]},
-  estoque: {type: Number, required: [true, "A quantidade em estoque do produto é obrigatória"]},
-  categoria: {type: String, required: [true, "A categoria do produto é obrigatória"]}
-}, {versionKey: false});
+const produtoSchema = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId
+    },
+
+    nome: {
+      type: String, 
+      required: [true, "O nome do produto é obrigatório"]
+    },
+    marca: {
+      type: String, 
+      required: [true, "A marca do produto é obrigatória"]
+    },
+
+    preco: {
+      type: Number, 
+      required: [true, "o preço do produto é obrigatório"],    
+    },
+
+    estoque: {
+      type: Number, 
+      required: [true, "A quantidade em estoque do produto é obrigatória"]},
+
+    categoria: {
+      type: String,
+      required: [true, "A categoria do produto é obrigatória"]
+    }
+      
+  }, 
+  {
+    versionKey: false}
+);
 
 const produto = mongoose.model("hardwares", produtoSchema);
 
