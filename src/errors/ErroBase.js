17740aecf4614fe4b323142a31423a243a8d4d88
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-04-19 16:18:58 -0300

    Finalizar função de erro base

--- src/errors/ErroBase.js
+++ src/errors/ErroBase.js
@@ -1,1 +1,5 @@
-function 
+function erroBase(req, res, next, mensagem = "Erro interno no servidor", status = 500) {
+  res.status(status).send({mensagem: mensagem, status: status});
+}
+
+export default erroBase;
